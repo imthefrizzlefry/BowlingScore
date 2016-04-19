f4552b716811c7dfdeede700740f71f90e2a6b98
imthefrizzlefry <imthefrizzlefry@gmail.com>
2016-04-19 16:54:28 -0700

    Added files via upload

---
+++ BowlingScore/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingScore
+{
+    public class Player
+    {
+        public List<int> PinCount { get; set; }
+        public string Name { get; set; }        
+        public int Score { get; set; }     
+        
+        public Player(string playerName)
+        {
+            Name = playerName;
+            Score = 0;
+            PinCount = new List<int>();
+        }   
+
+        public bool AddPinsToScore(int pinsStanding, int pinsDown)
+        {
+            if(pinsDown <= pinsStanding)
+            {
+                Console.WriteLine("{0} Knocked Down {1} pins!", Name, pinsDown);
+                PinCount.Add(pinsDown);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("{0} cannot knock down more than {1} pins.", Name, pinsStanding);
+                return false;
+            }
+        }      
+
+        public int CalculatePlayerScore()
+        {
+            Score = Scoring.ComputeScore(PinCount);
+            return Score;
+        }
+
+
+    }
+}
---
+++ BowlingScore/Scoring.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingScore
+{
+    public class Scoring
+    {
+        public static int ComputeScore(List<int> perBallScores)
+        {
+            int numberOfBallsThrown = perBallScores.Count;
+            int finalScore = 0;
+            bool continuingFrame = true;
+            int previousBallInFrame = 0;
+            int numberOfBallsPlayed=0; //each frame should count as 2 balls played
+
+            //stop execution if there are not enough scores provided
+            if (numberOfBallsThrown < 10) { return -1; }
+            
+            try {
+                for (int ballThrown = 0; (ballThrown < numberOfBallsThrown && finalScore < 300); ballThrown++)
+                {
+                    int currentScore = perBallScores[ballThrown];
+
+                    numberOfBallsPlayed++;
+
+                    if (currentScore == 10) // if there is a strike
+                    {
+                        finalScore +=  (currentScore + perBallScores[ballThrown + 1] + perBallScores[ballThrown + 2]); // add current score to next two balls
+
+                        numberOfBallsPlayed++; // strike counts as two balls played
+                    }
+
+                    else if (currentScore + previousBallInFrame >= 10) //if there is a spare
+                    {
+                        //break if the user knocked down too many balls on a spare
+                        if (currentScore + previousBallInFrame > 10) {
+                            return -1; }
+
+                        //increment current score plus the bonus round
+                        finalScore += currentScore + perBallScores[ballThrown + 1];
+                        
+                        //reset score counter for previous ball
+                        previousBallInFrame = 0;
+                        continuingFrame = !continuingFrame;
+                        
+                        //if this is the last ball of frame 10, increment the ball counter
+                        if(ballThrown+2 == numberOfBallsThrown) { ballThrown++; }
+                    }
+
+                    else if (currentScore < 10) // normal ball
+                    {
+                        //tally score
+                        finalScore += currentScore;
+                        // set the previous score to current score if it is zero, 
+                        // or reset it to zero if it has already been set
+                        previousBallInFrame = continuingFrame ? currentScore : 0;
+                        continuingFrame = !continuingFrame;
+                    }
+                    else // invalid input scenario
+                    {
+                        return -1;
+                    }
+                }
+
+                if(numberOfBallsPlayed < 20) 
+                { return -1; }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return -1; // either an extra play on spare or strike is missing
+            }
+
+            return finalScore;
+        }
+    }
+}
